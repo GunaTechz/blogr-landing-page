3ccfde229bf40c2e949780446b7553e7fc98ad5d
ragulpalanisamy <ragulpalanisamy1001@gmail.com>
2023-12-30 22:32:27 +0530

    feat: :sparkles: Added the desktop desgin for the footer

---
+++ clients/utils/Footer.js
@@ -0,0 +1,56 @@
+export const FproductsData = [
+    {
+        name : "Overview",
+        link : "#"
+    },
+    {
+        name : "Pricing",
+        link : "#"
+    },
+    {
+        name : "Marketplace",
+        link : "#"
+    },
+    {
+        name : "Features",
+        link : "#"
+    },
+    {
+        name : "Integrations",
+        link : "#"
+    }
+];
+
+export const FcompanyData = [
+    {
+        name : "About",
+        link : "#"
+    },
+    {
+        name : "Team",
+        link : "#"
+    },
+    {
+        name : "Blog",
+        link : "#"
+    },
+    {
+        name : "Careers",
+        link : "#"
+    }
+]
+
+export const FconnectData = [
+    {
+        name : "Contact",
+        link : "#"
+    },
+    {
+        name : "Newsletter",
+        link : "#"
+    },
+    {
+        name : "LinkedIn",
+        link : "#"
+    }
+]
