c9e9057aa60f5e314243c36f6e6917f4c86a6851
ragulpalanisamy <ragulpalanisamy1001@gmail.com>
2023-12-31 19:52:53 +0530

    feat: :sparkles: Added the design for future component

---
+++ clients/app/components/DesignforFuture.tsx
@@ -0,0 +1,25 @@
+import { designForFutureData } from '@/utils/Footer';
+import React from 'react';
+
+export default function DesignforFuture() {
+  return (
+    <div className='py-24'>
+      <div className='max-w-6xl mx-auto mt-10'>
+        <h1 className='text-4xl font-bold text-center font-overpass text-very-dark-blue'>Design for the future</h1>
+        <div className='flex flex-col md:flex-row items-center justify-center items-start'>
+          <div className='md:w-1/2 pr-8'>
+            {designForFutureData.map((item, index) => (
+              <div className='py-5' key={index}>
+                <h1 className='text-2xl font-bold text-start font-overpass text-very-dark-blue'>{item.title}</h1>
+                <p className='text-lg font-overpass text-justify text-grayish-blue pt-5'>{item.desc}</p>
+              </div>
+            ))}
+          </div>
+          <div className='md:w-1/2'>
+            <img src="/images/illustration-editor-desktop.svg" alt="" className='w-[130%] h-auto max-w-none' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- clients/utils/Footer.js
+++ clients/utils/Footer.js
@@ -27,3 +27,15 @@ export const footerData = [
       ],
     },
   ];
+
+
+  export const designForFutureData = [
+    {
+      title: 'Introducing an extensible editor',
+      desc: 'Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog.',
+    }, 
+    {
+      title: 'Robust content management',
+      desc: `Flexible content management enables users to easily move through posts. Increase the usability of your blog by adding customized categories, sections, format, or flow. With this functionlity you're in full control.`,
+    }
+  ]
